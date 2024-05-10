f3f80d8abca815684410b63895b97e1405b077b1
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2024-05-11 01:05:02 +0700

    create grid function

--- index.js
+++ index.js
@@ -7,16 +7,45 @@ const bot = mineflayer.createBot({
     username: "Pixel_Bot"
 })
 
+const testBot = mineflayer.createBot({
+    host: "localhost",
+    port: 52137,
+    username: "Test_Bot"
+})
+
 bot.loadPlugin(pathfinder)
+testBot.loadPlugin(pathfinder)
 
 bot.once("spawn", () => {
     bot.mcData = require("minecraft-data")(bot.version)
 })
 
-bot.on("chat", (username, message) => {
+bot.on("chat", async (username, message) => {
     const player = bot.players[username]
     if (message.toLowerCase() === "place") {
-        placeBlockAt(player.entity.position)
+        placeBlockAt(player.entity.position, true)
+        // let sourceBlock = bot.blockAt(bot.entity.position.offset(1, 0, 0))
+        // await testBot.placeBlock(sourceBlock, { x: 0, y: 1, z: 0 })
+        // testBot.chat("Place finished.")
+
+        // testBot.pathfinder.setMovements(new Movements(testBot, require("minecraft-data")(bot.version)))
+
+        // try {
+        //     await testBot.pathfinder.goto(new goals.GoalNear(player.entity.position.x, player.entity.position.y,
+        //                                                 player.entity.position.z, 0
+        //     ))
+        //     testBot.chat("Here!")
+        //     let sourceBlock = bot.blockAt(testBot.entity.position.offset(1, 0, 0))
+        //     await testBot.placeBlock(sourceBlock, { x: 0, y: 1, z: 0 })
+        //     testBot.chat("Placed!")
+        // } catch(e) {
+        //     // console.error(e)
+        // }
+        
+    }
+
+    if (message.toLowerCase() === "create grid") {
+        createGrid(3, 3, player.entity.position)
     }
 })
 
@@ -34,7 +63,7 @@ function holdItem(itemName) {
     return false
 }
 
-function placeBlockAt(p, faceVector={ x: 0, y: 1, z: 0 }, placeBlockName="stone") {
+async function placeBlockAt(p, backward=false, faceVector={ x: 0, y: 1, z: 0 }, placeBlockName="stone") {
     if (placing) {
         bot.chat("I am placing other blocks.")
         return
@@ -44,40 +73,43 @@ function placeBlockAt(p, faceVector={ x: 0, y: 1, z: 0 }, placeBlockName="stone"
     let position = p.clone()
     // Move To Nearby Block
     bot.pathfinder.setMovements(new Movements(bot, bot.mcData))
-    bot.pathfinder.setGoal(new goals.GoalNear(position.x, position.y, position.z, 0), false)
-
-    setTimeout(async () => {
-        // Moving
-        const t1 = new Date().getTime()
-        let t2 = new Date().getTime()
-        while(bot.pathfinder.isMoving()) {
-            t2 = new Date().getTime()
-            if (t2 - t1 > 10000) {
-                bot.chat("Too long...")
-                break
-            }
-
-            // bot.chat(bot.entity.position.distanceTo(position).toString())
-
-            await bot.waitForTicks(1)
-        }
-        bot.pathfinder.stop()
-        bot.pathfinder.setGoal(null)
+    await bot.pathfinder.goto(new goals.GoalNear(position.x, position.y, position.z, 0), false)
+    bot.pathfinder.stop()
+    bot.pathfinder.setGoal(null)
         
-        // Place block
-        if (!holdItem(placeBlockName)) return
+    // Place block
+    if (!holdItem(placeBlockName)) return
 
-        let sourceBlock = bot.blockAt(bot.entity.position.offset(1, 0, 0))
+    let sourceBlock
+    if (!backward) {
+        sourceBlock = bot.blockAt(bot.entity.position.offset(1, 0, 0))
+    }
+        
+    else {
+        sourceBlock = bot.blockAt(bot.entity.position.offset(-1, -1, 0))
+    }
 
-        try {
-            await bot.placeBlock(sourceBlock, faceVector)
+    placing = false
+
+    try {
+        await bot.placeBlock(sourceBlock, faceVector)
+    }
+    catch (err) {
+        if (!err.message.includes("Event") || !err.message.includes("did not fire within timeout")) {
+            console.log(err)
         }
-        catch (err) {
-            if (!err.message.includes("Event") || !err.message.includes("did not fire within timeout")) {
-                console.log(err)
-            }
-        }
+    }
+}
 
-        placing = false
-    }, 150)
+async function createGrid(row_num, col_num, sPos) {
+    const startPos = sPos.clone()
+    for (let r = 0; r < row_num; r++) {
+        let currentPos
+        for (let c = 0; c < col_num; c++) {
+            currentPos = startPos.offset(-c-1, r, 0)
+            await placeBlockAt(currentPos)
+        }
+        currentPos = startPos.offset(-col_num+1, r+1, 0)
+        await placeBlockAt(currentPos, true)
+    }
 }
