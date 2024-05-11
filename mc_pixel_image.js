4419fad8df8ccefe01daf23a10f71cb2a5a53995
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2024-05-11 15:31:44 +0700

    scan img function

--- mc_pixel_image.js
+++ mc_pixel_image.js
@@ -2,6 +2,34 @@ const sharp = require("sharp");
 const Jimp = require("jimp");
 
 async function readImg() {
-    await Jimp.read("./Cybord_Idle.png")
+    const image = await Jimp.read("shapes.png")
+
+    image.resize(32, 32)
+
+    image.write("edited-shapes.png")
+    
+    const pixelData = await scanImg(image)
+    console.log(pixelData)
+    
 }
+
+async function scanImg(image) {
+    let pixelData = []
+
+    await image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
+        let red = this.bitmap.data[idx + 0]
+        let green = this.bitmap.data[idx + 1];
+        let blue = this.bitmap.data[idx + 2];
+        let alpha = this.bitmap.data[idx + 3];
+
+        pixelData.push({
+            r: red,
+            g: green,
+            b: blue
+        })
+    })
+
+    return pixelData
+}
+
 readImg()
